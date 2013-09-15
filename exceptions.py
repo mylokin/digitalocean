35f4c0d260ce843ea74db1d71b43c8054ecc3207
Andrey Gubarev <mylokin@me.com>
2013-09-16 00:17:15 +0400

    fix exceptions

--- exceptions.py
+++ exceptions.py
@@ -13,4 +13,4 @@ class NotFound(Exception):
     status_code = 404
 
 
-status_codes = {e.status_code: e for e in locals().values() if inspect.isclass(e) and issubclass(e, Exception) and e.code}
+status_codes = {e.status_code: e for e in locals().values() if inspect.isclass(e) and issubclass(e, Exception) and e.status_code}
