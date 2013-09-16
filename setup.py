f17ae703054113609e07d56cad1afcd0a7b5baa5
Andrey Gubarev <mylokin@me.com>
2013-09-16 15:50:01 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(name='digitalocean',
+      version='0.1',
+      description='Digital Ocean API',
+      author='Andrey Gubarev',
+      author_email='mylokin@me.com',
+      url='https://github.com/mylokin/digitalocean',
+      packages=['digitalocean'],
+      install_requires=['requests'],
+     )
