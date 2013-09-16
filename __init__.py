6de8231892231e16c361fd192b36a4ce2a4089f6
Andrey Gubarev <mylokin@me.com>
2013-09-16 13:06:08 +0400

    rename api to session

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from .utils import (
 
 class Event(models.Event):
     def __init__(self, api, **event):
-        self.__api = api
+        self.session = api
         super(Event, self).__init__(**event)
 
         self.Event = functools.partial(Event, api)
@@ -20,18 +20,18 @@ class Event(models.Event):
 
     @require(['id'])
     def __call__(self):
-        return self.Event(**self.__api.event(self.id))
+        return self.Event(**self.session.event(self.id))
 
 
 class Size(models.Size):
     def __init__(self, api, **size):
-        self.__api = api
+        self.session = api
         super(Size, self).__init__(**size)
 
         self.Size = functools.partial(Size, api)
 
     def __iter__(self):
-        for size in self.__api.sizes():
+        for size in self.session.sizes():
             yield self.Size(**size)
 
 
@@ -40,7 +40,7 @@ class Droplet(models.Droplet):
         'enable_backups', 'disable_backups')
 
     def __init__(self, api, **droplet):
-        self.__api = api
+        self.session = api
         super(Droplet, self).__init__(**droplet)
 
         self.Event = functools.partial(Event, api)
@@ -48,10 +48,10 @@ class Droplet(models.Droplet):
 
     @require(['id'])
     def __call__(self):
-        return self.Droplet(**self.__api.droplet(self.id))
+        return self.Droplet(**self.session.droplet(self.id))
 
     def __iter__(self):
-        for droplet in self.__api.droplets():
+        for droplet in self.session.droplets():
             yield self.Droplet(**droplet)
 
     def __getattr__(self, name):
@@ -59,7 +59,7 @@ class Droplet(models.Droplet):
             return self.Event(**event_id(action(self.id)))
 
         if name in self.ACTIONS:
-            action = getattr(self.__api, 'droplet_{}'.format(name))
+            action = getattr(self.session, 'droplet_{}'.format(name))
             if self.id:
                 return handler(action)
             else:
@@ -70,7 +70,7 @@ class Droplet(models.Droplet):
 
     @require(['name', 'size_id', 'image_id', 'region_id'])
     def new(self, ssh_key_ids=None, private_networking=None):
-        droplet = self.__api.droplet_new(self.name, self.size_id, self.image_id, self.region_id,
+        droplet = self.session.droplet_new(self.name, self.size_id, self.image_id, self.region_id,
             ssh_key_ids=ssh_key_ids,
             private_networking=private_networking)
         event = self.Event(id=droplet.pop('event_id'))
@@ -79,24 +79,24 @@ class Droplet(models.Droplet):
 
     @require(['id'])
     def destroy(self, scrub_data=None):
-        return self.Event(**event_id(self.__api.droplet_destroy(self.id, scrub_data=scrub_data)))
+        return self.Event(**event_id(self.session.droplet_destroy(self.id, scrub_data=scrub_data)))
 
     @require(['id'])
     def snapshot(self, name=None):
-        return self.Event(**event_id(self.__api.droplet_snapshot(self.id, name=name)))
+        return self.Event(**event_id(self.session.droplet_snapshot(self.id, name=name)))
 
     @require(['id'])
     def resize(self, size_id):
-        return self.Event(**event_id(self.__api.droplet_resize(self.id, size_id)))
+        return self.Event(**event_id(self.session.droplet_resize(self.id, size_id)))
 
     @require(['id'])
     def restore(self, image_id):
-        return self.Event(**event_id(self.__api.droplet_restore(self.id, image_id)))
+        return self.Event(**event_id(self.session.droplet_restore(self.id, image_id)))
 
     @require(['id'])
     def rebuild(self, image_id):
-        return self.Event(**event_id(self.__api.droplet_rebuild(self.id, image_id)))
+        return self.Event(**event_id(self.session.droplet_rebuild(self.id, image_id)))
 
     @require(['id'])
     def rename(self, name):
-        return self.Event(**event_id(self.__api.droplet_rename(self.id, name)))
+        return self.Event(**event_id(self.session.droplet_rename(self.id, name)))
